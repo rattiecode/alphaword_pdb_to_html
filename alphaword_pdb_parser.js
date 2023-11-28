4e91e72ebe5a15fae183d02653f799dd37d024c6
rattie <rattiecoding@gmail.com>
2023-11-27 22:07:35 -0700

    Parse PDB, convert to MD, display in Editor.md

---
+++ alphaword_pdb_parser.js
@@ -0,0 +1,188 @@
+// This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+(function (root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["kaitai-struct/KaitaiStream"], factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory(require("kaitai-struct/KaitaiStream"));
+  } else {
+    root.AlphawordPdb = factory(root.KaitaiStream);
+  }
+})(typeof self !== "undefined" ? self : this, function (KaitaiStream) {
+  var AlphawordPdb = (function () {
+    AlphawordPdb.Alignment = Object.freeze({
+      LEFT: 0,
+      CENTER: 1,
+      RIGHT: 2,
+
+      0: "LEFT",
+      1: "CENTER",
+      2: "RIGHT",
+    });
+
+    function AlphawordPdb(_io, _parent, _root) {
+      this._io = _io;
+      this._parent = _parent;
+      this._root = _root || this;
+
+      this._read();
+    }
+    AlphawordPdb.prototype._read = function () {
+      this.fileName = KaitaiStream.bytesToStr(
+        KaitaiStream.bytesTerminate(this._io.readBytes(32), 0, false),
+        "ISO-8859-1"
+      );
+      this.idk = this._io.readBytes(28);
+      this.docType = KaitaiStream.bytesToStr(
+        this._io.readBytes(4),
+        "ISO-8859-1"
+      );
+      this.creatorApp = KaitaiStream.bytesToStr(
+        this._io.readBytes(4),
+        "ISO-8859-1"
+      );
+      this.idk2 = this._io.readBytes(46);
+      this.paragraphCount1 = this._io.readU4be();
+      this.paragraphCount2 = this._io.readU4be();
+      this.characterCountIdk = this._io.readU4be();
+      this.seriouslyIdk = this._io.readU4be();
+      this.idk3 = this._io.readU1();
+      this.fontLabelLength = this._io.readU2be();
+      this.fontLabels = this._io.readBytes(this.fontLabelLength);
+      this.sequences = [];
+      var i = 0;
+      while (!this._io.isEof()) {
+        this.sequences.push(new SequenceType(this._io, this, this._root));
+        i++;
+      }
+    };
+
+    var SequenceType = (AlphawordPdb.SequenceType = (function () {
+      function SequenceType(_io, _parent, _root) {
+        this._io = _io;
+        this._parent = _parent;
+        this._root = _root || this;
+
+        this._read();
+      }
+      SequenceType.prototype._read = function () {
+        this.typeIndicator = this._io.readU1();
+        switch (this.typeIndicator) {
+          case 1:
+            this.innerType = new TextStyle(this._io, this, this._root);
+            break;
+          case 5:
+            this.innerType = new ParagraphFormatting(
+              this._io,
+              this,
+              this._root
+            );
+            break;
+          default:
+            this.innerType = this._io.readU1();
+            break;
+        }
+      };
+
+      return SequenceType;
+    })());
+
+    var StyleFlags = (AlphawordPdb.StyleFlags = (function () {
+      function StyleFlags(_io, _parent, _root) {
+        this._io = _io;
+        this._parent = _parent;
+        this._root = _root || this;
+
+        this._read();
+      }
+      StyleFlags.prototype._read = function () {
+        this.flags = this._io.readU1();
+      };
+      Object.defineProperty(StyleFlags.prototype, "isUnderlined", {
+        get: function () {
+          if (this._m_isUnderlined !== undefined) return this._m_isUnderlined;
+          this._m_isUnderlined = (this.flags & 4) != 0;
+          return this._m_isUnderlined;
+        },
+      });
+      Object.defineProperty(StyleFlags.prototype, "isItalic", {
+        get: function () {
+          if (this._m_isItalic !== undefined) return this._m_isItalic;
+          this._m_isItalic = (this.flags & 2) != 0;
+          return this._m_isItalic;
+        },
+      });
+      Object.defineProperty(StyleFlags.prototype, "isBold", {
+        get: function () {
+          if (this._m_isBold !== undefined) return this._m_isBold;
+          this._m_isBold = (this.flags & 1) != 0;
+          return this._m_isBold;
+        },
+      });
+
+      return StyleFlags;
+    })());
+
+    var TextStyle = (AlphawordPdb.TextStyle = (function () {
+      function TextStyle(_io, _parent, _root) {
+        this._io = _io;
+        this._parent = _parent;
+        this._root = _root || this;
+
+        this._read();
+      }
+      TextStyle.prototype._read = function () {
+        this.length = this._io.readU2be();
+        this.fontIndex = this._io.readU1();
+        this.fontSize = this._io.readU1();
+        this.idk = this._io.readU1();
+        this.styleFlags = new StyleFlags(this._io, this, this._root);
+        this.text = KaitaiStream.bytesToStr(
+          this._io.readBytes(this.length),
+          "ISO-8859-1"
+        );
+      };
+
+      return TextStyle;
+    })());
+
+    var ParagraphFormatting = (AlphawordPdb.ParagraphFormatting = (function () {
+      function ParagraphFormatting(_io, _parent, _root) {
+        this._io = _io;
+        this._parent = _parent;
+        this._root = _root || this;
+
+        this._read();
+      }
+      ParagraphFormatting.prototype._read = function () {
+        this.idk = this._io.readBytes(5);
+        this.alignment = this._io.readU1();
+        this.idk2 = this._io.readBytes(6);
+      };
+
+      return ParagraphFormatting;
+    })());
+
+    var FontLabel = (AlphawordPdb.FontLabel = (function () {
+      function FontLabel(_io, _parent, _root) {
+        this._io = _io;
+        this._parent = _parent;
+        this._root = _root || this;
+
+        this._read();
+      }
+      FontLabel.prototype._read = function () {
+        this.idk = this._io.readU2be();
+        this.fileName = KaitaiStream.bytesToStr(
+          this._io.readBytesTerm(0, false, true, true),
+          "ISO-8859-1"
+        );
+      };
+
+      return FontLabel;
+    })());
+
+    return AlphawordPdb;
+  })();
+  return AlphawordPdb;
+});
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const fileInput = document.getElementById("file_input");
-console.log("fileInput: ", fileInput);
-const utf8decoder = new TextDecoder(); // default 'utf-8' or 'utf8'
-let lastArrayBuffer = null;
 
 async function fileChangeHandler(fileChangeEvent) {
   const file = fileChangeEvent.target.files[0];
@@ -10,14 +7,47 @@ async function fileChangeHandler(fileChangeEvent) {
   lastArrayBuffer = arrayBuffer;
   console.log("What is arrayBuffer? ", arrayBuffer);
 
-  let nameBuffer = new Uint8Array(arrayBuffer, 0, 32);
-  console.log("What is nameBuffer?", nameBuffer);
+  var parsedPDB = new AlphawordPdb(new KaitaiStream(arrayBuffer));
+  console.log("What is parsedPDB?", parsedPDB);
 
-  const indexOfFirstNullByte = nameBuffer.indexOf(0);
-  nameBuffer = nameBuffer.slice(0, indexOfFirstNullByte);
+  let currentParagraph = [];
+  let paragraphs = [];
+  function processTextStyle(value) {
+    let text = value.text;
+    if (value.styleFlags.isItalic) {
+      text = `_${text}_`;
+    }
+    if (value.styleFlags.isBold) {
+      text = `**${text}**`;
+    }
+    if (value.styleFlags.isUnderlined) {
+      text = `<u>${text}</u>`;
+    }
+    currentParagraph.push(text);
+  }
+  function processParagraph(value) {
+    currentParagraph = [];
+    paragraphs.push(currentParagraph);
+  }
 
-  const documentName = utf8decoder.decode(nameBuffer);
-  console.log("What is documentName?", documentName);
+  parsedPDB.sequences.forEach(function (item) {
+    if (item.typeIndicator === 5) {
+      processParagraph(item.innerType);
+    } else if (item.typeIndicator === 1) {
+      processTextStyle(item.innerType);
+    } else {
+      console.warn(
+        `Unsupported sequence type: "${item.typeIndicator}".\n(The program didn't recognize this bit of your file for conversion.)\nIf you'd like us to add support for your PDB file, please send it to rattiecoding@gmail.com.`
+      );
+    }
+  });
+
+  //console.log("What is paragraphs after parsing?", paragraphs);
+  const paragraphsAsStrings = paragraphs.map(function (paragraph) {
+    return paragraph.join("");
+  });
+  const output = paragraphsAsStrings.join("\n\n");
+  editor.setValue(output);
 }
 
 fileInput.addEventListener("change", fileChangeHandler);
