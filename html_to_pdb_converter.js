6d7450fc93fb89129885c0eb8d4a7d326c2d5002
rattie <rattiecoding@gmail.com>
2024-03-04 20:23:07 -0700

    Get access to paragraph alignment in HTML input

--- html_to_pdb_converter.js
+++ html_to_pdb_converter.js
@@ -1,168 +1,308 @@
 const tests = [
   {
     input: "<p></p>",
-    output: [],
+    output: {
+      children: [],
+    },
   },
   {
     input: "<p>0 - plain</p>",
-    output: [{ text: "0 - plain" }],
+    output: {
+      children: [{ text: "0 - plain" }],
+    },
   },
   {
     input: "<h1>Header 1</h1>",
-    output: [
-      {
-        text: "Header 1",
-        isBold: true,
-        isUnderlined: true,
-      },
-    ],
+    output: {
+      children: [
+        {
+          text: "Header 1",
+          isBold: true,
+          isUnderlined: true,
+        },
+      ],
+    },
   },
   {
     input: "<p><strong>1 - bold</strong></p>",
-    output: [
-      {
-        text: "1 - bold",
-        isBold: true,
-      },
-    ],
+    output: {
+      children: [
+        {
+          text: "1 - bold",
+          isBold: true,
+        },
+      ],
+    },
   },
   {
     input:
       "<p>5 - this text is normal, <strong>this text is bold</strong> , <em>this text is italic</em> , <u>this is underlined</u></p>",
-    output: [
-      {
-        text: "5 - this text is normal, ",
-      },
-      {
-        text: "this text is bold",
-        isBold: true,
-      },
-      {
-        text: " , ",
-      },
-      {
-        text: "this text is italic",
-        isItalic: true,
-      },
-      {
-        text: " , ",
-      },
-      {
-        text: "this is underlined",
-        isUnderlined: true,
-      },
-    ],
+    output: {
+      children: [
+        {
+          text: "5 - this text is normal, ",
+        },
+        {
+          text: "this text is bold",
+          isBold: true,
+        },
+        {
+          text: " , ",
+        },
+        {
+          text: "this text is italic",
+          isItalic: true,
+        },
+        {
+          text: " , ",
+        },
+        {
+          text: "this is underlined",
+          isUnderlined: true,
+        },
+      ],
+    },
   },
   {
     input:
       "<p><strong>15 - this text is bold with <em>italic in the middle with <u>underline</u> in the middle</em> of that.</strong></p>",
-    output: [
-      {
-        text: "15 - this text is bold with ",
-        isBold: true,
-      },
-      {
-        text: "italic in the middle with ",
-        isBold: true,
-        isItalic: true,
-      },
-      {
-        text: "underline",
-        isBold: true,
-        isItalic: true,
-        isUnderlined: true,
-      },
-      {
-        text: " in the middle",
-        isBold: true,
-        isItalic: true,
-      },
-      {
-        text: " of that.",
-        isBold: true,
-      },
-    ],
+    output: {
+      children: [
+        {
+          text: "15 - this text is bold with ",
+          isBold: true,
+        },
+        {
+          text: "italic in the middle with ",
+          isBold: true,
+          isItalic: true,
+        },
+        {
+          text: "underline",
+          isBold: true,
+          isItalic: true,
+          isUnderlined: true,
+        },
+        {
+          text: " in the middle",
+          isBold: true,
+          isItalic: true,
+        },
+        {
+          text: " of that.",
+          isBold: true,
+        },
+      ],
+    },
   },
   {
     input:
       "<p><u>16 - this text is underlined with</u> <strong><u>bold in the middle with</u> <em><u>italic</u></em> <u>in the middle</u></strong> <u>of that.</u></p>",
-    output: [
-      {
-        text: "16 - this text is underlined with",
-        isUnderlined: true,
-      },
-      {
-        text: " ",
-      },
-      {
-        text: "bold in the middle with",
-        isBold: true,
-        isUnderlined: true,
-      },
-      {
-        text: " ",
-        isBold: true,
-      },
-      {
-        text: "italic",
-        isBold: true,
-        isItalic: true,
-        isUnderlined: true,
-      },
-      {
-        text: " ",
-        isBold: true,
-      },
-      {
-        text: "in the middle",
-        isBold: true,
-        isUnderlined: true,
-      },
-      {
-        text: " ",
-      },
-      {
-        text: "of that.",
-        isUnderlined: true,
-      },
-    ],
+    output: {
+      children: [
+        {
+          text: "16 - this text is underlined with",
+          isUnderlined: true,
+        },
+        {
+          text: " ",
+        },
+        {
+          text: "bold in the middle with",
+          isBold: true,
+          isUnderlined: true,
+        },
+        {
+          text: " ",
+          isBold: true,
+        },
+        {
+          text: "italic",
+          isBold: true,
+          isItalic: true,
+          isUnderlined: true,
+        },
+        {
+          text: " ",
+          isBold: true,
+        },
+        {
+          text: "in the middle",
+          isBold: true,
+          isUnderlined: true,
+        },
+        {
+          text: " ",
+        },
+        {
+          text: "of that.",
+          isUnderlined: true,
+        },
+      ],
+    },
   },
   {
     input:
       "<p><em>17 - this text is italic with <u>underline in the middle with</u></em> <strong><em><u>bold</u></em></strong> <em><u>in the middle</u> of that.</em></p>",
-    output: [
-      {
-        text: "17 - this text is italic with ",
-        isItalic: true,
-      },
-      {
-        text: "underline in the middle with",
-        isItalic: true,
-        isUnderlined: true,
-      },
-      {
-        text: " ",
-      },
-      {
-        text: "bold",
-        isBold: true,
-        isItalic: true,
-        isUnderlined: true,
-      },
-      {
-        text: " ",
-      },
-      {
-        text: "in the middle",
-        isItalic: true,
-        isUnderlined: true,
-      },
-      {
-        text: " of that.",
-        isItalic: true,
-      },
-    ],
+    output: {
+      children: [
+        {
+          text: "17 - this text is italic with ",
+          isItalic: true,
+        },
+        {
+          text: "underline in the middle with",
+          isItalic: true,
+          isUnderlined: true,
+        },
+        {
+          text: " ",
+        },
+        {
+          text: "bold",
+          isBold: true,
+          isItalic: true,
+          isUnderlined: true,
+        },
+        {
+          text: " ",
+        },
+        {
+          text: "in the middle",
+          isItalic: true,
+          isUnderlined: true,
+        },
+        {
+          text: " of that.",
+          isItalic: true,
+        },
+      ],
+    },
   },
+  {
+    input:
+      "<p>The following is an empty em: <em></em>. <span>This is a span.</span> <b>This is bold (b).</b> <i>This is italic (i).</i></p>",
+    output: {
+      children: [
+        {
+          text: "The following is an empty em: ",
+        },
+        {
+          text: ". This is a span. ",
+        },
+        {
+          text: "This is bold (b).",
+          isBold: true,
+        },
+        {
+          text: "This is italic (i).",
+          isItalic: true,
+        },
+      ],
+    },
+  },
+  {
+    input: "<h2>This is an H2 header.</h2>",
+    output: {
+      children: [
+        {
+          text: "This is an H2 header.",
+          isUnderlined: true,
+        },
+      ],
+    },
+  },
+  {
+    input: "<h3>This is an H3 header.</h3>",
+    output: {
+      children: [
+        {
+          text: "This is an H3 header.",
+        },
+      ],
+    },
+  },
+
+  {
+    input: "<p>This text is left-aligned.</p>",
+    output: {
+      children: [
+        {
+          text: "This text is left-aligned.",
+        },
+      ],
+    },
+  },
+  {
+    input: '<p class="ql-align-center">This text is centered by Quill.</p>',
+    output: {
+      align: "center",
+      children: [
+        {
+          text: "This text is centered by Quill.",
+        },
+      ],
+    },
+  },
+  {
+    input: '<p style="text-align: center;">This text is centered by style.</p>',
+    output: {
+      align: "center",
+      children: [
+        {
+          text: "This text is centered by style.",
+        },
+      ],
+    },
+  },
+  {
+    input: '<p class="ql-align-right">This text is right-aligned by Quill.</p>',
+    output: {
+      align: "right",
+      children: [
+        {
+          text: "This text is right-aligned by Quill.",
+        },
+      ],
+    },
+  },
+  {
+    input:
+      '<p style="text-align: right;">This text is right-aligned by style.</p>',
+    output: {
+      align: "right",
+      children: [
+        {
+          text: "This text is right-aligned by style.",
+        },
+      ],
+    },
+  },
+  {
+    input: '<p class="ql-align-justify">This text is justified by Quill.</p>',
+    output: {
+      align: "justified",
+      children: [
+        {
+          text: "This text is justified by Quill.",
+        },
+      ],
+    },
+  },
+  {
+    input:
+      '<p style="text-align: justify;">This text is justified by style.</p>',
+    output: {
+      align: "justified",
+      children: [
+        {
+          text: "This text is justified by style.",
+        },
+      ],
+    },
+  },
+
+  // TODO: Take spans with no style properties and merge with text nodes with no properties
 ];
 
 const getDanaStyledTextNodesFromDomNode = function (
@@ -170,7 +310,10 @@ const getDanaStyledTextNodesFromDomNode = function (
   parentStatuses
 ) {
   let result = [];
+  // Receives parentStatuses and copies their properties to a new Object
+  // parentStatuses can be undefined, it will not give the empty Object additional properties, and will remain empty
   const statuses = Object.assign({}, parentStatuses);
+
   const nodes = topLevelNode.childNodes;
 
   if (topLevelNode.nodeName === "H1") {
@@ -198,13 +341,40 @@ const getDanaStyledTextNodesFromDomNode = function (
   return result;
 };
 
+const getAlignmentFromNode = function (domNode) {
+  // console.log("What is domNode.style?", domNode.style);
+  const quillAlignmentRegex = /ql-align-(justify|right|center|left)/;
+  const regexMatch = quillAlignmentRegex.exec(domNode.className);
+  // This is a Javascript ternary expression, an in-line if condition. Is regexMatch truthy?
+  // Pre question mark is the test, post question mark is truthy branch, post is falsey
+  const quillClassName = regexMatch ? regexMatch[1] : undefined;
+
+  return quillClassName || domNode.style.textAlign || undefined;
+};
+
+const getWholeEnchilada = function (topLevelNode) {
+  return {
+    align: getAlignmentFromNode(topLevelNode),
+    children: getDanaStyledTextNodesFromDomNode(topLevelNode),
+  };
+};
+
 tests.forEach(function (test) {
+  // Creates an empty div element
   const testDomNode = document.createElement("div");
+  // Fills div with string of HTML from each test's input property
+  // Hydrate the HTML from the text of the input
   testDomNode.innerHTML = test.input;
+  // Gets a handle on the first child node in hydrated contents
   const topLevelNode = testDomNode.children[0];
-  const output = getDanaStyledTextNodesFromDomNode(topLevelNode);
+  // Enter it into the test function
+  const output = getWholeEnchilada(topLevelNode);
 
   const outputAsJson = JSON.stringify(output);
   const expectedOutputAsJson = JSON.stringify(test.output);
-  console.assert(expectedOutputAsJson === outputAsJson, test.input);
+  // Use stringify to compare the equivalency of the objects by comparing their internal properties in the form of string
+  if (expectedOutputAsJson !== outputAsJson) {
+    const errorMessage = `Test failed: ${test.input}\n\tExpected: ${expectedOutputAsJson}\n\tActual: ${outputAsJson}`;
+    console.error(errorMessage);
+  }
 });
